acd911c26e33055c01256270dbd914f737e572b7
mariabeatrizmo <mariabeatrizmo@gmail.com>
2022-11-28 13:08:23 +0000

    example

--- README.md
+++ README.md
@@ -23,3 +23,15 @@ In the `include` directory can be found a file named `config.hpp`.
 - `log_name` specifies the name of the log file. If left empty the log file will be "log_D.F" where "D" is the datetime of creation of the log file and "F" it's extension (depends on the log_type choosen).
 - `reset_pid_per_op` is a flag that specifies if the PIP is always rechecked at every operation intercepted (!=0), or if it's not necessary to do so (==0).
 
+### Example 
+``` shell
+$ git clone git@github.com:mariabeatrizmo/Profiler.git
+$ cd Profiler
+$ mkdir build; cd build
+$ cmake ..
+$ cmake --build .
+$ export PROFILING_DIR=$(pwd)/libprofiler.so
+$ cd ../test
+$ g++ -g -o main main.cpp -ldl
+$ LD_PRELOAD=$PROFILING_DIR ./main
+```
---
+++ test/main.cpp
@@ -0,0 +1,51 @@
+#include <fcntl.h>
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int main(){
+    int pos = 0;
+    int buffsize = 256;
+    bool cont = true;
+
+    int fd = open("/usr/local/lib/python3.8/dist-packages/tensorflow/python/keras/engine/data_adapter.py", O_RDONLY, S_IRWXU);
+    int fd_1 = open("/home/gsd/logs/copy.py", O_CREAT | O_RDWR, S_IRWXU);
+
+    if(fd>0){ 
+        unsigned char* buffer = (unsigned char*) malloc(buffsize);
+
+        while(cont){ //if(cont){
+            memset(buffer, 0, buffsize);
+            int pos3 = (int) read(fd, buffer, buffsize);
+            if(pos3>0){
+                write(fd_1,buffer,pos3);
+            }
+            else cont = false;
+        }
+
+        close(fd);
+        close(fd_1);
+
+        free(buffer);
+    }
+
+    int N=5;
+    void *ptr = mmap ( NULL, N*sizeof(int),PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    if(ptr == MAP_FAILED){ 
+        printf("Mapping Failed\n");
+        return 1;
+    }
+
+    int err = munmap(ptr, 10*sizeof(int));
+    if(err != 0){
+        printf("UnMapping Failed\n");
+        return 1;
+    }
+    
+    return 0;
+}
